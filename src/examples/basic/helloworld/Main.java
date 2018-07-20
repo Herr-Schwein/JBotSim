b601926e051afc4ba5f22c2c712b0b67e8c6acc4
Denis Arrivault <denis.arrivault@lif.univ-mrs.fr>
2018-07-20 17:48:13 +0200

    Add the application pluggin that produces one executable binary for each example

--- src/examples/basic/helloworld/Main.java
+++ src/examples/basic/helloworld/Main.java
@@ -3,7 +3,7 @@ package examples.basic.helloworld;
 import jbotsim.Topology;
 import jbotsimx.ui.JViewer;
 
-public class HelloWorld {
+public class Main {
     public static void main(String[] args){
         Topology tp = new Topology();
         new JViewer(tp);
